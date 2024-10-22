47caef903e0a8be61883159c7adb4a70b9bca738
SirMac <sir.mattew.holmes@gmail.com>
2024-10-21 21:49:13 -0700

    fixed error

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   envDir: './environment',
-  plugins: [react()],
-  base: '/dist/',
-  build: {
-    outDir: 'dist',
-  },
+  plugins: [react()]
 });
