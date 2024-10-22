d9940403e0b345b9f65b9f6547496a1c9ece8900
SirMac <sir.mattew.holmes@gmail.com>
2024-10-21 21:40:14 -0700

    added render

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,13 @@ import './index.css';
 import App from './App.tsx';
 import CandidateSearch from './pages/CandidateSearch.tsx';
 import SavedCandidates from './pages/SavedCandidates.tsx';
-import ErrorPage from './pages/ErrorPage.tsx';
+// import ErrorPage from './pages/ErrorPage.tsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    errorElement: <ErrorPage />,
+    // errorElement: <ErrorPage />,
     children: [
       {
         index: true,
--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   envDir: './environment',
   plugins: [react()],
+  base: '/dist/',
+  build: {
+    outDir: 'dist',
+  },
 });
